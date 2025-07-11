5fad68363a0fad58e2eb67e4feb1f2926046e7d5
Maximus Champion <maximuschampion2002@gmail.com>
2025-07-11 14:25:15 +0300

    created schemas for accounts

---
+++ src/schemas/accounts.py
@@ -0,0 +1,58 @@
+from datetime import datetime, date
+from typing import Optional
+
+from pydantic.v1 import BaseModel, constr
+
+
+class UserSchema(BaseModel):
+    id: int
+    email: constr(max_length=255)
+    hashed_password: constr(max_length=255)
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+    group_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserProfileSchema(BaseModel):
+    id: int
+    user_id: int
+    first_name: Optional[constr(max_length=100)] = None
+    last_name: Optional[constr(max_length=100)] = None
+    avatar: Optional[constr(max_length=255)] = None
+    gender: Optional[constr(max_length=10)] = None
+    date_of_birth: Optional[date] = None
+    info: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+
+class RefreshTokenSchema(BaseModel):
+    id: int
+    user_id: int
+    token: constr(max_length=255)
+    expires_at: datetime
+
+
+class PasswordResetTokenSchema(BaseModel):
+    id: int
+    user_id: int
+    token: constr(max_length=255)
+    expires_at: datetime
+
+
+class ActivationTokenSchema(BaseModel):
+    id: int
+    user_id: int
+    token: constr(max_length=255)
+    expires_at: datetime
+
+
+class UserGroupSchema(BaseModel):
+    id: int
+    name: constr(max_length=50)
